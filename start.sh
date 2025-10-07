74ba0138d38a5803346b77376d33d7a706bd9d22
Lakshay Jindal <ljindal275@gmail.com>
2025-10-07 23:03:41 +0530

    Added Railway related content

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -o errexit
-cd main
+
 echo "📦 Collecting static files..."
 python manage.py collectstatic --noinput
 
@@ -8,4 +8,4 @@ echo "🧩 Applying migrations..."
 python manage.py migrate --noinput
 
 echo "🚀 Starting Daphne ASGI server..."
-daphne -b 0.0.0.0 -p 8000 main.asgi:application
+daphne -b 0.0.0.0 -p ${PORT:-8000} main.asgi:application
