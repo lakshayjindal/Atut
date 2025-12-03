ac255bdee3172862d87baa6380bda0f6f4d8bfb3
Lakshay Jindal <ljindal275@gmail.com>
2025-12-03 23:35:44 +0530

    change

--- plans/apps.py
+++ plans/apps.py
@@ -4,7 +4,3 @@ from django.apps import AppConfig
 class PlansConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "plans"
-    def ready(self):
-        from .models import SiteSettings
-        if not SiteSettings.objects.exists():
-            SiteSettings.objects.create()
